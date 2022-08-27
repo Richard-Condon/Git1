06a7275b6d704cc45225387ce028e4106a40470b
lcd <lcd's email>
2022-08-27 09:31:21 +0800

    first commit

---
+++ git-demo1/src/main/java/lcd/GitTest.java
@@ -0,0 +1,11 @@
+package lcd;
+
+/**
+ * @athor lcdstart
+ * @create 2022-08-27 9:26
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git.");
+    }
+}
